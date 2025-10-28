c6033011186c9fa386af0d3e431f1fd9546de9a7
Alagulakshmi63 <alagulakshmi6369@gmail.com>
2025-10-28 05:19:48 -0700

    Add files via upload

---
+++ Family member.py
@@ -0,0 +1,8 @@
+Name="Alagulakshmi"
+Fathername="Mariyappan"
+Mothername="Thangaprakashi"
+Brothername="Arulkumar"
+print("my name is",Name)
+print("my fathername is",Fathername)
+print("my mothername is",Mothername)
+print("my brothername is",Brothername)
