27d0bf8b3449a089e92b15d0b1cfdd807762730e
Alagulakshmi63 <alagulakshmi6369@gmail.com>
2025-10-29 05:06:55 -0700

    Add files via upload

---
+++ Alagu.py
@@ -0,0 +1,16 @@
+Name=Alagulakshmi
+Age=23
+Height=133
+Weight=55
+City=Tenkasi
+Student=False
+Mark=348
+Schoolname=GovernmentHr.Sec.School
+print("My Name Is",Name)
+print("My Age",Age)
+print("My Height",133)
+print("My Weight",55)
+print("My City",City)
+print("Student",Student)
+print("Mark",Mark)
+print("Schoolname",Schoolname)
