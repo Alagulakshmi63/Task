07a90996bc9917eee39b1d9ef1f5e3dac85989b0
Alagulakshmi63 <alagulakshmi6369@gmail.com>
2025-11-03 03:46:56 -0800

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,32 @@
+print("print 1 to 20")
+for i in range(1,20):
+    print(i)
+print("print even numbers")
+for i in range(2,50,2):
+    print(i)
+print("print odd numbers")
+for i in range(1,50,3):
+    print(i)
+print("square of numbers")
+for i in range(1,16):
+    print(i**2)
+print("cube of numbers")
+for i in range(1,11):
+    print(i**3)
+print("10 down to reverse order")
+for i in range(10,0,-1):
+    print(i)
+print("multiblication table of 5")
+for i in range(1,11):
+    print(f"5*{i}={5*i}")
+print("all character string one by one")
+for char in "alagulakshmi":
+    print(char)
+print("divisable by 3 between 1 and 30")
+for i in range(1,31):
+    if i%3==0:
+        print(i)
+    
+    
+    
+    
